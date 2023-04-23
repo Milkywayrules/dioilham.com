50684ce3ef404ec31d85dfe059a9a924f25478ac
Dio Ilham Djatiadi <apajabole1@gmail.com>
2023-04-24 01:32:40 +0700

    fix: umami analytics not sync

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,29 +10,32 @@ const interVariable = Inter()
 
 export default function App({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <ThemeProvider attribute="class">
-      <main className={interVariable.className}>
-        <Component {...pageProps} />
-        <Analytics />
+    <>
+      <Script
+        async
+        defer
+        data-website-id="2a4410ee-2d77-42a7-ae81-88244b97e73b"
+        src="https://umami-production-79bd.up.railway.app/script.js"
+      />
 
-        {/* <Script
+      <ThemeProvider attribute="class">
+        <main className={interVariable.className}>
+          <Component {...pageProps} />
+          <Analytics />
+
+          {/* <Script
           data-website-id="2a4410ee-2d77-42a7-ae81-88244b97e73b"
           src="https://umami-production-79bd.up.railway.app/script.js"
           strategy="lazyOnload"
         /> */}
 
-        {/* <script
+          {/* <script
           async
           src="https://umami-production-79bd.up.railway.app/script.js"
           data-website-id="2a4410ee-2d77-42a7-ae81-88244b97e73b"
         ></script> */}
-        <Script
-          async
-          defer
-          data-website-id="2a4410ee-2d77-42a7-ae81-88244b97e73b"
-          src="https://umami-production-79bd.up.railway.app/script.js"
-        />
-      </main>
-    </ThemeProvider>
+        </main>
+      </ThemeProvider>
+    </>
   )
 }
